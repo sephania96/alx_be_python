d07aa44f8cea9e6a5a6d1c5302f73e85b56d1df8
sephania96 <koutanyi@yahoo.com>
2024-06-17 21:31:34 -0700

    future_age_calculator.py

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+#request for users age
+fig = int(input("How old are you?"))
+#the int is used to type cast the string output of the input function
+age = (2050 - 2023) + fig
+print(f"In 2050, you will be {age} years old")
