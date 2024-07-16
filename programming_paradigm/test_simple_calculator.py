a5f71cfe86991e5c24d0d27b76b429e08f4849b0
sephania96 <koutanyi@yahoo.com>
2024-07-16 06:06:12 +0100

    update

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -8,18 +8,25 @@ class Testsimple_calculator(unittest.TestCase):
 
     def test_add(self):
         self.assertEqual(self.simple_calculator.add(10, 5), 15)
+        self.assertEqual(self.simple_calculator.add(-10, 5), -5)
+        self.assertEqual(self.simple_calculator.add(10, -5), 5)
 
     def test_subtract(self):
         self.assertEqual(self.simple_calculator.subtract(10, 5), 5)
+        self.assertEqual(self.simple_calculator.subtract(10, -5), 15)
+        self.assertEqual(self.simple_calculator.subtract(-10, 5), -15)
 
     def test_multiply(self):
         self.assertEqual(self.simple_calculator.multiply(10, 5), 50)
+        self.assertEqual(self.simple_calculator.multiply(-10, 5), -50)
+        self.assertEqual(self.simple_calculator.multiply(10, -5), -50)
+        self.assertEqual(self.simple_calculator.multiply(-10, -5), 50)
     
     def test_divide(self):
         self.assertEqual(self.simple_calculator.divide(10, 5), 2)
+        self.assertEqual(self.simple_calculator.divide(-10, 5), -2)
+        self.assertEqual(self.simple_calculator.divide(10, -5), -2)
 
-        with self.assertRaises(ValueError):
-             self.simple_calculator.divide(10, 0)
 
 
 if __name__ == '__main__':
