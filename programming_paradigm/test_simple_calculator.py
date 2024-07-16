41ff5fbacc32ff2bfe61e5e7c35090c2883c20ba
sephania96 <koutanyi@yahoo.com>
2024-07-16 06:09:50 +0100

    update

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -4,28 +4,28 @@ from simple_calculator import SimpleCalculator
 class Testsimple_calculator(unittest.TestCase):
 
     def setUp(self):
-        self.simple_calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_add(self):
-        self.assertEqual(self.simple_calculator.add(10, 5), 15)
-        self.assertEqual(self.simple_calculator.add(-10, 5), -5)
-        self.assertEqual(self.simple_calculator.add(10, -5), 5)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(10, 5), 15)
+        self.assertEqual(self.calc.add(-10, 5), -5)
+        self.assertEqual(self.calc.add(10, -5), 5)
 
     def test_subtract(self):
-        self.assertEqual(self.simple_calculator.subtract(10, 5), 5)
-        self.assertEqual(self.simple_calculator.subtract(10, -5), 15)
-        self.assertEqual(self.simple_calculator.subtract(-10, 5), -15)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(10, -5), 15)
+        self.assertEqual(self.calc.subtract(-10, 5), -15)
 
     def test_multiply(self):
-        self.assertEqual(self.simple_calculator.multiply(10, 5), 50)
-        self.assertEqual(self.simple_calculator.multiply(-10, 5), -50)
-        self.assertEqual(self.simple_calculator.multiply(10, -5), -50)
-        self.assertEqual(self.simple_calculator.multiply(-10, -5), 50)
+        self.assertEqual(self.calc.multiply(10, 5), 50)
+        self.assertEqual(self.calc.multiply(-10, 5), -50)
+        self.assertEqual(self.calc.multiply(10, -5), -50)
+        self.assertEqual(self.calc.multiply(-10, -5), 50)
     
     def test_divide(self):
-        self.assertEqual(self.simple_calculator.divide(10, 5), 2)
-        self.assertEqual(self.simple_calculator.divide(-10, 5), -2)
-        self.assertEqual(self.simple_calculator.divide(10, -5), -2)
+        self.assertEqual(self.calc.divide(10, 5), 2)
+        self.assertEqual(self.calc.divide(-10, 5), -2)
+        self.assertEqual(self.calc.divide(10, -5), -2)
 
 
 
